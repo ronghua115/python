a4f915b34866a8b0b821b110ea484f47ade4ec7c
Ronghua Huang <ronghua115@gmail.com>
2019-10-31 11:09:15 -0400

    remote moves back and forth the route defined

--- parse_geo_file.py
+++ parse_geo_file.py
@@ -3,8 +3,8 @@ import datetime
 import json
 import os
 from datetime import timedelta
-
 import pyodbc
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config_file", help="configuration file of geo location data", type=str)
@@ -27,17 +27,22 @@ conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server 
                       ';UID=' + username + ';PWD=' + password)
 cursor = conn.cursor()
 
+# datafile location
+datafile_location = all_info['datafile_location']
+
 remotes = all_info['remotes']
 for remote in remotes:
     # geo data file
-    file_name = os.path.join(__location__ + "/datafile", remote['geo_data_name'])
+    file_name = os.path.join(__location__ + "/" + datafile_location, remote['geo_data_name'])
     # remote info
-    time_stamp_str = remote['time_stamp']
-    time_stamp = datetime.datetime.strptime(time_stamp_str, '%Y-%m-%d %H:%M:%S')
+    time_stamp_str = remote['start_date']
+    time_stamp_start = datetime.datetime.strptime(time_stamp_str, '%Y-%m-%d')
+    time_stamp_end = time_stamp_start + timedelta(days=remote['duration_day'])
     system_id = remote['system_id']
     remote_id = remote['remote_id']
-    geo_freq = remote['geo_freq']  # seconds
-    altitude = '100.00'
+    geo_freq = remote['geo_freq_seconds']
+    # all geo info
+    geo_data = []
     # parse geo data file
     try:
         fp = open(file_name, 'r', encoding='utf-8')
@@ -65,13 +70,40 @@ for remote in remotes:
             longitude_seconds = longitude[longitude_minutes_index + 1:-1]
             longitude_decimal = format(float(longitude_degree) + float(longitude_minutes) / 60 + float(longitude_seconds) / 3600, '.6f')
 
-            # print(f'{time_stamp}, {system_id}, {remote_id}: {latitude_decimal}{latitude_direction}, {longitude_decimal}{longitude_direction}')
-            cursor.execute("INSERT INTO [NEURONSim].[dbo].[RemoteLocation]([Timestamp], [SystemId], [RemoteId], [Latitude],\
-                            [Longitude], [LatDirection], [LongDirection], [Altitude]) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",\
-                            time_stamp, system_id, remote_id, latitude_decimal, longitude_decimal, latitude_direction,\
-                            longitude_direction, altitude)
-            conn.commit()
-
-            time_stamp += timedelta(seconds=geo_freq)
+            # save all geo data
+            geo_data_row = [latitude_decimal, longitude_decimal, latitude_direction, longitude_direction]
+            geo_data.append(geo_data_row)
     finally:
         fp.close()
+
+    # insert into database for one remote
+    # print(len(geo_data))
+    time_stamp = time_stamp_start
+    geo_data_index = 0
+    geo_data_direction = 'f'
+    while time_stamp < time_stamp_end:
+        latitude_decimal = geo_data[geo_data_index][0]
+        longitude_decimal = geo_data[geo_data_index][1]
+        latitude_direction = geo_data[geo_data_index][2]
+        longitude_direction = geo_data[geo_data_index][3]
+        altitude = random.uniform(33000, 42000)  # feet
+        # print(f'{time_stamp}, {system_id}, {remote_id}: {latitude_decimal}{latitude_direction}, {longitude_decimal}{longitude_direction}')
+        # insert one record into database
+        cursor.execute("INSERT INTO [NEURONSim].[dbo].[RemoteLocation]([Timestamp], [SystemId], [RemoteId], [Latitude],\
+                        [Longitude], [LatDirection], [LongDirection], [Altitude]) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",\
+                        time_stamp, system_id, remote_id, latitude_decimal, longitude_decimal, latitude_direction,\
+                        longitude_direction, altitude)
+        conn.commit()
+
+        if geo_data_direction == 'f':
+            geo_data_index += 1
+            if geo_data_index == len(geo_data):
+                geo_data_direction = 'b'
+                geo_data_index -= 2
+        elif geo_data_direction == 'b':
+            geo_data_index -= 1
+            if geo_data_index == -1:
+                geo_data_direction = 'f'
+                geo_data_index += 2
+
+        time_stamp += timedelta(seconds=geo_freq)
